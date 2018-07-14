d03be2d2b6e9d431a33b9adcb4ac5d5b087e7b9a
AyushBajra <realayush@gmail.com>
2018-07-15 00:44:48 +0545

    Rendering template remaining

--- esp.py
+++ esp.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, flash
 from flask_wtf import Form
 from wtforms import DecimalField, SubmitField, validators, ValidationError, SubmitField
 import pickle
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = 'development key'
@@ -12,38 +14,15 @@ class InputForm(Form):
     moisture = DecimalField("Moisture: ", [validators.Required()])
     temperature = DecimalField("Temperature (%): ",[validators.Required()])
     humidity = DecimalField("Humidity (%): ",[validators.Required()])
- 
-@app.route('/yoESP', methods = ['POST','GET'])
+
+
+@app.route('/yoESP', methods = ['POST'])
 def ESPHandler():
-	if request.method == 'POST':
-		content = request.get_json()
-		moisture = content['moisture']
-		temperature = content['temperature']
-		humidity = content['humidity']
-		
-		return 'NodeMCU'
-		'''
-		input_values = [[(moisture), (temperature), (humidity)]]
-		result = loaded_model.predict(input_values)
-		if result == 0:
-			crop = 'Corn'
-		elif result == 1:
-			crop = 'Maize'
-		elif result == 2:
-			crop = 'Potato'
-		elif result == 3:
-			crop = 'Rice'
-		elif result == 4:
-			crop = 'Sugarcane'
-		elif result == 5:
-			crop = 'Tomato'
-		else:
-			crop = 'Wheat'
-		
-		return crop
-		'''
-	else:
-		return 'NodeMCU'
+	content = request.get_json()
+	moisture = content['moisture']
+	temperature = content['temperature']
+	humidity = content['humidity']	
+	return 'NodeMCU'
 		
 
 @app.route('/', methods =['GET', 'POST'])
@@ -74,11 +53,24 @@ def home():
                 crop = 'Tomato'
             else:
                 crop = 'Wheat'
-            return render_template('thatCrop.html', crop = crop )
+            return render_template('thatCrop.html', crop = crop)
     if request.method == 'GET':
         return render_template('form.html', form = form)
 
-
+@app.route('/readycode', methods =['POST','GET'])
+def ready():
+	if request.method =='POST':
+		#r = requests.post("http://192.168.100.98:80/body")
+		url = 'http://192.168.100.98:80/body'
+		payload={'fruit':'apple'}
+		headers = {'content-type': 'application/json'}
+		r = requests.post(url, data = json.dumps(payload), headers=headers)
+		return render_template('intermediate.html')
+	else:
+		return 'No'
+		
+		
+		
 if __name__ == "__main__":	
 	app.run(host = '0.0.0.0', port = 8090)
 	
--- first.ino
+++ first.ino
@@ -3,10 +3,14 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-
+#include <String.h>
 
 const char *ssid = "prometheus_wlink";  //ENTER YOUR WIFI SETTINGS
 const char *password = "7550882670";
+
+bool state = false;
+
+ESP8266WebServer server(80);
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,11 +34,23 @@ void setup() {
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  
+  server.on("/body",handleBody);
+  server.begin();
+  Serial.println("Server Listening");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+   
+  server.handleClient();
+
+  if (state==1){
+    Serial.println("It's ready.");
+
+   if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
    JsonObject& JSONencoder = JSONbuffer.createObject();  
         
@@ -66,54 +82,27 @@ void loop() {
  
  }
 
-  delay(30000);
-
-
-
-  
-    /**if (WiFi.status() == WL_CONNECTED) {
-      StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-      JsonObject& JSONencoder = JSONbuffer.createObject();  
-
-      JSONencoder["sensorType"] = "Temperature";
- 
-      JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
-      values.add(20); //Add value to array
-      values.add(21); //Add value to array
-      values.add(23); //Add value to array
-   
-      JsonArray& timestamps = JSONencoder.createNestedArray("timestamps"); //JSON array
-      timestamps.add("10:10"); //Add value to array
-      timestamps.add("10:20"); //Add value to array
-      timestamps.add("10:30"); //Add value to array
-
-      char JSONmessageBuffer[300];
-      JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-      Serial.println(JSONmessageBuffer);
-
-      HTTPClient http;    //Declare object of class HTTPClient
- 
-      http.begin("http://127.0.0.1:5000/postjson");      //Specify request destination
-      http.addHeader("Content-Type", "application/json");  //Specify content-type header
-   
-      int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-      String payload = http.getString();                                        //Get the response payload
-   
-      Serial.println(httpCode);   //Print HTTP return code
-      Serial.println(payload);    //Print request response payload
-   
-      http.end();  //Close connection
-   
-    } else {
-   
-      Serial.println("Error in WiFi connection");
-   
+    
+    state = false;
     }
-   
-    delay(30000);  //Send a request every 30 seconds
-    **/
   
-       
-   
 }
+
+void handleBody(){
+     if (server.hasArg("plain")== false){ //Check if body received
+ 
+            server.send(200, "text/plain", "Body not received");
+            return;   
+      }
+ 
+      String message = server.arg("plain");
+             message += "\n";
+      server.send(200, "text/plain", message);
+      Serial.println(state); // here
+      Serial.println(message);
+      state = true;
+      Serial.println(state);
+      
+  }
+
 
